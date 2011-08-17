6239c69f7d41ae72977819f4e5a3dc3310aab7f1
Michael Christenson II <michael@rebelhold.com>
2011-08-16 20:06:21 -0700

    initial initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc6'
+
+gem 'mongoid'
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0.rc"
+  gem 'coffee-rails', "~> 3.1.0.rc"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+group :test do
+  gem 'ruby-debug19', :require => 'ruby-debug'
+  
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialize::Application.config.secret_token = '53b3ec1bf2490d76f3aab47c857ba5ac661f737b1dd9e24f3191f8f79c91f638f45d16b23456d19486482b2680baaad305766c4db271393e042681d24544a3dc'
